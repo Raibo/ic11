ab91d9afc78121e632866f0d9c31bf2ae240a931
Eduard Vlasov <driver.edro@gmail.com>
2024-09-22 19:40:58 +0200

    more yolo

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var input = "Pin FabElectro = d0;\r\nPin DialElectro = d3;\r\n\r\nvoid Main()\r\n{   \r\n    while()\r\n    {\r\n        yield;\r\n        \r\n        if (FabElectro.IsSet && DialElectro.IsSet)\r\n        {            \r\n            if (!FabElectro.Activate)\r\n            {\r\n                FabElectro.ClearMemory = true;\r\n                return;\r\n            }\r\n            \r\n            var craftsDone = FabElectro.ExportCount;\r\n            var craftsTarget = DialElectro.Setting;\r\n            \r\n            var craftsLeft = craftsTarget - craftsDone;\r\n            \r\n            DialElectro.Setting = craftsLeft;\r\n            FabElectro.ClearMemory = true;\r\n            \r\n            if (craftsLeft <= 0)\r\n            {\r\n                DialElectro.Setting = 1;\r\n                FabElectro.Activate = false;\r\n            }\r\n        }\r\n    }\r\n}";
-
+        var file = "examples/test.ic11";
+        var input = System.IO.File.ReadAllText(file);
+         
         AntlrInputStream inputStream = new AntlrInputStream(input);
         Ic11Lexer lexer = new Ic11Lexer(inputStream);
         CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
