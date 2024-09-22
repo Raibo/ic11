27cbd57e94d8467c0d9b68315d3be641555efa9f
Eduard Vlasov <driver.edro@gmail.com>
2024-09-22 22:45:24 +0200

    manual codegen

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.IO;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 class Program
 {
     static void Main(string[] args)
     {
         var file = "examples/test.ic11";
-        var input = System.IO.File.ReadAllText(file);
+        var input = File.ReadAllText(file);
          
         AntlrInputStream inputStream = new AntlrInputStream(input);
         Ic11Lexer lexer = new Ic11Lexer(inputStream);
@@ -15,6 +17,7 @@ class Program
 
         var tree = parser.program(); // Assuming 'program' is the entry point of your grammar
 
+        // print
         Console.WriteLine(tree.ToStringTree(parser));
     }
 }
--- grammars/README.md
+++ grammars/README.md
@@ -1,5 +1,19 @@
+# Setup
+
 ```
 pip install antlr4-tools
+```
 
+# Debug GUI
+
+```
+cd grammars # this dir
 antlr4-parse Ic11.g4 program -gui ../examples/test.ic11
+```
+
+# Generate 
+
+```
+cd grammars # this dir
+antlr4 -o ../generated Ic11.g4
 ```
