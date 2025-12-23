c6d5d1c4536348a6c1a0b132eb64d13de60a876b
Fabian Neundorf <CommodoreFabianus@gmx.de>
2025-12-23 01:23:33 +0100

    feat: support unbalanced block/statement in if
    This also makes it possible to use "else if" with blocks.

---
+++ src/ic11.Tests/IfStatementTest.cs
@@ -0,0 +1,270 @@
+namespace ic11.Tests;
+
+using ic11.Emulator;
+
+[TestClass]
+public sealed class IfStatementTest
+{
+    [TestMethod]
+    public void TestIfElseBlock()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+            }
+
+            real Test(p) {
+                if (p == 1) {
+                    return 42;
+                } else {
+                    return 1337;
+                }
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+    }
+
+    [TestMethod]
+    public void TestIfElseStatement()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+            }
+
+            real Test(p) {
+                if (p == 1)
+                    return 42;
+                else
+                    return 1337;
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+    }
+
+    [TestMethod]
+    public void TestIfElseBlockStatement()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+            }
+
+            real Test(p) {
+                if (p == 1) {
+                    return 42;
+                } else
+                    return 1337;
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+    }
+
+    [TestMethod]
+    public void TestIfElseStatementBlock()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+            }
+
+            real Test(p) {
+                if (p == 1)
+                    return 42;
+                else {
+                    return 1337;
+                }
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+    }
+
+    [TestMethod]
+    public void TestIfElseIfBlock()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+                D.C = Test(3);
+            }
+
+            real Test(p) {
+                if (p == 1) {
+                    return 42;
+                } else if (p == 2) {
+                    return 1337;
+                } else {
+                    return 4711;
+                }
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+        Assert.AreEqual(4711, dev.GeneralProperties["C"]);
+    }
+
+    [TestMethod]
+    public void TestIfElseIfStatement()
+    {
+        var code = @"
+            pin D d0;
+
+            void Main()
+            {
+                D.A = Test(1);
+                D.B = Test(2);
+                D.C = Test(3);
+            }
+
+            real Test(p) {
+                if (p == 1)
+                    return 42;
+                else if (p == 2)
+                    return 1337;
+                else
+                    return 4711;
+            }
+        ";
+
+        var compileText = Program.CompileText(code);
+        Console.WriteLine(compileText);
+
+        var program = compileText.Split("\n");
+
+        Emulator emulator = new(1);
+        emulator.LoadProgram(program);
+
+        var limit = 1000;
+        while (!emulator.Stopped && --limit > 0)
+        {
+            emulator.Run(1);
+            emulator.PrintSummary();
+        }
+
+        emulator.PrintSummary();
+
+        var dev = emulator.Devices[0]!;
+        Assert.AreEqual(42, dev.GeneralProperties["A"]);
+        Assert.AreEqual(1337, dev.GeneralProperties["B"]);
+        Assert.AreEqual(4711, dev.GeneralProperties["C"]);
+    }
+}
